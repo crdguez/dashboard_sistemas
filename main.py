652fec2cdd5cb0022c4fd89e84f07d3e319c1439
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-09-18 09:43:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ st.write(sistema_a_latex(sist))
 
 
 # st.write(discusion_solucion(sist2, k=k, resol=True))
+st.write(discusion_solucion(sist, k=k, resol=True)['solucion_latex'])
 st.latex(discusion_solucion(sist, k=k, resol=True)['solucion_latex'])
 
 
