e8c98c3945bf2d53c546fd07da1811186c740d02
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-09-18 09:29:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ st.title("Resolución de Sistemas de 2º bachillerato")
 sist=[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)]
 st.write("Dado el sistema:")
 st.write(sistema_a_latex(sist))
-st.latex(latex(solve(sist)))
+# st.latex(latex(solve(sist)))
 
-st.write(sistema_a_latex(sist))
+
 # st.write(discusion_solucion(sist2, k=k, resol=True))
 # st.latex(discusion_solucion(sist2, k=k, resol=True))
 
