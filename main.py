e1e21f072461623869fdf2e5edb9bb2fee83d87c
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-09-18 08:47:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit as st
 # from funciones import *
 from sympy import *
 
@@ -10,6 +11,6 @@ a, b, c, d, k = symbols('a b c d k', real = True)
 st.title("Resolución de Sistemas de 2º bachillerato")
 
 sist2=[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)]
-st.tex(latex(solve(sist2)))
+st.latex(latex(solve(sist2)))
 
 
