553c488c8dba6d370c35b5db613be09ed8e854ab
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-09-18 08:35:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import streamlit as st
 from funciones import *
 from sympy import *
 
-init_session()
+x, y, z, t = symbols('x y z t')
 
-a, b, c, d = symbols('a b c d', real = True)
+a, b, c, d, k = symbols('a b c d k', real = True)
 
 
 st.title("Resolución de Sistemas de 2º bachillerato")
 
 sist2=[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)]
-st.write(discusion_solucion(sist2))
+st.write(solve(sist2))
 
 
