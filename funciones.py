b4ee3b07c449d85f50cdd852fb9af2582bfc77a2
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-09-19 13:12:18 +0200

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -125,7 +125,7 @@ def discusion_solucion(sist, k=k, resol=True) :
 
         solucion_latex += r"$\left|A\right|={}={} ".format(latex(A).replace("[","|").replace("]","|"),latex(A.det()))
         
-#         solucion_md.append(r"aquí $\left|A\right|={}={} ".format(latex(A).replace("[","|").replace("]","|"),latex(A.det())))
+        solucion_md.append(r"$\left|A\right|={}={} ".format(latex(A).replace("[","|").replace("]","|"),latex(A.det())))
         if solve(A.det()) :
             solucion_latex += r"\to \left|A\right|=0 \quad si \quad {} = {}$".format(k,latex(solve(A.det())))
             solucion_md.append(r"\to \left|A\right|=0 \quad si \quad {} = {}$".format(k,latex(solve(A.det()))))
