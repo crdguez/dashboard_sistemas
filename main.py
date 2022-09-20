ed9846dcaeeaf5f621224df7db124c5cb41cab23
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-09-20 07:40:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,21 @@ a, b, c, d, k = symbols('a b c d k', real = True)
 st.title("Resolución de Sistemas de 2º bachillerato")
 
 st.info("Escribe el sistema en formato sympy. **Ejemplo:** *[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)]*")
-sist=S(st.text_input("Sistema", value="[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)])"))
+sist=st.text_input("Sistema", value="[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)])")
 
+st.write(sist)
 # sist=[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)]
 
-st.write("Dado el sistema:")
-st.write(sistema_a_latex(sist))
-# st.latex(latex(solve(sist)))
+# st.write("Dado el sistema:")
+# st.write(sistema_a_latex(sist))
+# # st.latex(latex(solve(sist)))
 
 
-# st.write(discusion_solucion(sist2, k=k, resol=True))
-for linea in discusion_solucion(sist, k=k, resol=True)['solucion_markdown'] :
-  st.write(linea)
+# # st.write(discusion_solucion(sist2, k=k, resol=True))
+# for linea in discusion_solucion(sist, k=k, resol=True)['solucion_markdown'] :
+#   st.write(linea)
 
-# st.write(discusion_solucion(sist, k=k, resol=True)['solucion_markdown'])
-st.latex(discusion_solucion(sist, k=k, resol=True)['solucion_latex'])
+# # st.write(discusion_solucion(sist, k=k, resol=True)['solucion_markdown'])
+# st.latex(discusion_solucion(sist, k=k, resol=True)['solucion_latex'])
 
 
