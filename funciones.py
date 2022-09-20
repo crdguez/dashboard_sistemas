8888d9acfc410774d53db3076e48ad4af8b5f60b
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-09-20 07:25:14 +0200

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -109,7 +109,7 @@ def discusion_solucion(sist, k=k, resol=True) :
                 pprint("{} --> {} = {}".format(s[2],s[0],s[1]))
                 solucion_latex += r"\begin{itemize}"
                 solucion_latex += r"\item ${} \to {} = {}$".format(latex(s[2]),s[0],latex(s[1])).replace('[','(').replace(']',')')
-                solucion_md.append("                - ${} \\to {} = {}$".format(latex(s[2]),s[0],latex(s[1])).replace('[','(').replace(']',')'))
+                solucion_md.append(" ${} \\to {} = {}$".format(latex(s[2]),s[0],latex(s[1])).replace('[','(').replace(']',')'))
                 txt_md +="  \n              * ${} \\to {} = {}$".format(latex(s[2]),s[0],latex(s[1])).replace('[','(').replace(']',')')
                 solucion_latex += r"\end{itemize}"
 
