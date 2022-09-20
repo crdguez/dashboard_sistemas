09b275f72e7e0f7218e1742c2e52fcb04f4d56e9
Crdguez <31032709+crdguez@users.noreply.github.com>
2022-09-20 07:42:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ sist=S(st.text_input("Sistema", value="[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)]"))
 st.write(sist)
 # sist=[Eq(y+z,40),Eq(x,y+10),Eq(y-3*z,0)]
 
-# st.write("Dado el sistema:")
-# st.write(sistema_a_latex(sist))
-# # st.latex(latex(solve(sist)))
+st.write("Dado el sistema:")
+st.write(sistema_a_latex(sist))
+# st.latex(latex(solve(sist)))
 
 
-# # st.write(discusion_solucion(sist2, k=k, resol=True))
-# for linea in discusion_solucion(sist, k=k, resol=True)['solucion_markdown'] :
-#   st.write(linea)
+# st.write(discusion_solucion(sist2, k=k, resol=True))
+for linea in discusion_solucion(sist, k=k, resol=True)['solucion_markdown'] :
+  st.write(linea)
 
-# # st.write(discusion_solucion(sist, k=k, resol=True)['solucion_markdown'])
-# st.latex(discusion_solucion(sist, k=k, resol=True)['solucion_latex'])
+# st.write(discusion_solucion(sist, k=k, resol=True)['solucion_markdown'])
+st.latex(discusion_solucion(sist, k=k, resol=True)['solucion_latex'])
 
 
